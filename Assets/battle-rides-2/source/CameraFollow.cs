015faa203b7893a953d448b98a7671f28bacd8f4
Vexille <guilhermevv@gmail.com>
2017-06-14 22:09:05 +0200

    Initial commit

---
+++ Assets/battle-rides-2/source/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform Target;
+    public float FollowVelocity;
+
+    private Vector3 _distanceOffset;
+    private void Start() {
+        _distanceOffset = Target.position - transform.position;
+    }
+
+    private void Update() {
+        var fakePosition = transform.position + _distanceOffset;
+        var targetDirection = (Target.transform.position - fakePosition);
+
+        float interpVelocity = targetDirection.magnitude * FollowVelocity;
+
+        var targetPos = fakePosition + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos - _distanceOffset, 0.25f);
+    }
+
+    //void FixedUpdate() {
+    //    if (target) {
+    //        Vector3 posNoZ = transform.position;
+    //        posNoZ.z = target.transform.position.z;
+
+    //        Vector3 targetDirection = (target.transform.position - posNoZ);
+
+    //        interpVelocity = targetDirection.magnitude * 5f;
+
+    //        targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+    //        transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+    //    }
+    //}
+}
---
+++ Assets/battle-rides-2/source/CarController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    public Rigidbody FrontLeftWheel;
+    public Rigidbody FrontRightWheel;
+
+    public float Speed = 10f;
+
+    private float _motorInput;
+    //private float _steeringInput;
+
+    public void FixedUpdate() {
+        float motor = Speed * Input.GetAxis("Vertical");
+
+        var leftForce = FrontLeftWheel.transform.forward * motor;
+        var rightForce = FrontRightWheel.transform.forward * motor;
+
+        FrontLeftWheel.AddForce(FrontLeftWheel.transform.position + leftForce);
+        FrontRightWheel.AddForce(FrontRightWheel.transform.position + rightForce);
+
+        DrawDebugLines(leftForce, rightForce);
+    }
+
+    private void DrawDebugLines(Vector3 leftForce, Vector3 rightForce) {
+        Debug.DrawLine(FrontLeftWheel.transform.position, FrontLeftWheel.transform.position + leftForce.normalized);
+        Debug.DrawLine(FrontRightWheel.transform.position, FrontRightWheel.transform.position + rightForce.normalized);
+
+        Debug.DrawLine(FrontLeftWheel.transform.position, FrontLeftWheel.transform.position + FrontLeftWheel.transform.forward, Color.blue);
+        Debug.DrawLine(FrontRightWheel.transform.position, FrontRightWheel.transform.position + FrontRightWheel.transform.forward, Color.blue);
+    }
+}
