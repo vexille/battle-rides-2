c31b5e65250bee9724453837ee3e8cccc9d96174
Vexille <guilhermevv@gmail.com>
2017-06-16 23:15:09 +0200

    A car that car steer! And can't skid!

--- Assets/battle-rides-2/source/CarController.cs
+++ Assets/battle-rides-2/source/CarController.cs
@@ -4,24 +4,51 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 
+    public Transform Steering;
     public Rigidbody FrontLeftWheel;
     public Rigidbody FrontRightWheel;
 
-    public float Speed = 10f;
+    public float Speed = 30f;
+    public float MaxTurningAngle = 15f;
 
     private float _motorInput;
-    //private float _steeringInput;
+    private float _steeringInput;
+    private Rigidbody _rigidbody;
+
+    private void Awake() {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        _motorInput = Speed * 100f * Input.GetAxis("Vertical");
+        _steeringInput = MaxTurningAngle * Input.GetAxis("Horizontal");
+
+        Steering.localRotation = Quaternion.Euler(0f, _steeringInput, 0f);
+    }
 
     public void FixedUpdate() {
-        float motor = Speed * Input.GetAxis("Vertical");
+        var steeringDirection = Steering.forward;
+        var leftForce = steeringDirection * _motorInput;
+        var rightForce = steeringDirection * _motorInput;
+        //var mainForce = Steering.forward * _motorInput;
 
-        var leftForce = FrontLeftWheel.transform.forward * motor;
-        var rightForce = FrontRightWheel.transform.forward * motor;
+        FrontLeftWheel.AddForce(leftForce);
+        FrontRightWheel.AddForce(rightForce);
+        //_rigidbody.AddForce(mainForce);
 
-        FrontLeftWheel.AddForce(FrontLeftWheel.transform.position + leftForce);
-        FrontRightWheel.AddForce(FrontRightWheel.transform.position + rightForce);
+        UpdateFriction();
 
         DrawDebugLines(leftForce, rightForce);
+    }
+
+    private void UpdateFriction() {
+        Vector3 impulse = _rigidbody.mass * -GetLateralVelocity();
+        _rigidbody.AddForce(impulse, ForceMode.Impulse);
+    }
+
+    private Vector3 GetLateralVelocity() {
+        var rightNormal = transform.right;
+        return Vector3.Dot(rightNormal, _rigidbody.velocity) * rightNormal;
     }
 
     private void DrawDebugLines(Vector3 leftForce, Vector3 rightForce) {
@@ -30,5 +57,21 @@ public class CarController : MonoBehaviour {
 
         Debug.DrawLine(FrontLeftWheel.transform.position, FrontLeftWheel.transform.position + FrontLeftWheel.transform.forward, Color.blue);
         Debug.DrawLine(FrontRightWheel.transform.position, FrontRightWheel.transform.position + FrontRightWheel.transform.forward, Color.blue);
+
+        //var mainVelocity = _rigidbody.velocity;
+        //Debug.DrawLine(transform.position, transform.position + mainVelocity, Color.red);
+    }
+
+    private void OnGUI() {
+        var mainVelocity = _rigidbody.velocity;
+        //var leftVelocity = FrontLeftWheel.velocity;
+        //var rightVelocity = FrontRightWheel.velocity;
+        GUIStyle style = new GUIStyle();
+        style.fontStyle = FontStyle.Bold;
+        style.normal.textColor = Color.yellow;
+
+        GUILayout.Label(string.Format("Main speed: {0:0.000} ({1:0.#}, {2:0.#}, {3:0.#})", mainVelocity.magnitude, mainVelocity.x, mainVelocity.y, mainVelocity.z), style);
+        //GUILayout.Label(string.Format("LWheel speed: {0:0.000} ({1:0.#}, {2:0.#}, {3:0.#})", leftVelocity.magnitude, leftVelocity.x, leftVelocity.y, leftVelocity.z), style);
+        //GUILayout.Label(string.Format("RWheel speed: {0:0.000} ({1:0.#}, {2:0.#}, {3:0.#})", rightVelocity.magnitude, rightVelocity.x, rightVelocity.y, rightVelocity.z), style);
     }
 }
